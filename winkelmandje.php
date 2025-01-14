b32bc719e83ef215a04c85a820e08fbaa343abda
Wouter-HAN <WGP.martens@student.han.nl>
2025-01-14 15:14:02 +0100

    oefenen

---
+++ winkelmandje.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eetcafé 't Vette Bekkie</title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/huisstijlVetteBekkie2025.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header"><h1>Welkom bij Eetcafé <span class="media_uiting aandacht">'t Vette Bekkie</span></h1></div>
+        <div class="menu">
+            <ul>
+                <li><a href="#">Aanbiedingen</a></li>
+                <li><a href="#">Over Ons</a></li>
+                <li>
+                    <a href="#">Menukaart</a>
+                    <ul>
+                        <li><a href="#">Eten</a></li>
+                        <li><a href="#">Drinken</a></li>
+                    </ul>
+                </li>
+                <li><a href="winkelmandje.php">winkelmandje</a></li>
+                <li><a href="#">Inloggen</a></li>
+            </ul>
+        </div>
+        <div class="reclame_links reclame">
+            <img src="img/side_burger_1.gif" alt="aanbieding 1">
+            <img src="img/side_burger_2.gif" alt="aanbieding 2">
+            <img src="img/side_burger_3.gif" alt="aanbieding 3">
+        </div>
+        <div class="main"><section><h1>Aanbiedingen</h1>
+        <div class="bestelling">
+<h1>Bestelling Overzicht</h1>
+                <table class="Overzicht-Bestelling">
+                    <thead>
+                        <tr>
+                            <th>Pizza</th>
+                            <th>Aantal</th>
+                            <th>Prijs</th>
+                            <th>Totaal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Vegetarisch</td>
+                            <td colspan="3"><label for="Adres">Adres gegevens</label>
+                                 </td>
+                            <td><input type="Adres" id="Adres" name="Adres" placeholder="Adres gegevens"></td>
+                            <td>€8,75</td>
+                            <td>€8,50</td>
+                        </tr>
+                        <tr>
+                            <td>turkse pizza doner</td>
+                            <td><button class="min">-</button>2<button class="plus">+</button></td>
+                            <td>€7,50</td>
+                            <td>€18,00</td>
+                        </tr>
+                        <tr>
+                            <td>Meat lover</td>
+                            <td><button class="min">-</button>4<button class="plus">+</button></td>
+                            <td>€12.00</td>
+                            <td>€48.00</td>
+                        </tr>
+                        <tr class="totaal">
+                            <td colspan="3">Subtotaal</td>
+                            <td>€74.50</td>
+                        </tr>
+                        <tr class="totaal">
+                            <td colspan="3">Verzendkosten</td>
+                            <td>€3,50</td>
+                        </tr>
+                        <tr class="totaal">
+                            <td colspan="3">Totaal</td>
+                            <td>€78,00</td>
+                        </tr>
+                        <tr class="totaal">
+                            <td colspan="3"><label for="Adres">Adres gegevens</label>
+                                 </td>
+                            <td><input type="Adres" id="Adres" name="Adres" placeholder="Adres gegevens"></td>
+                            </tr>
+                            <td colspan="3"><label for="Password">Naam</label>
+                                 </td>
+                            <td><input type="Naam" id="Naam" name="Naam" placeholder="Naam"></td>
+                        
+                    </tbody>
+                </table>
+
+
+                
+                <div class="order-actie">
+                    <button class="knoppenbestelling">Bestellen</button>
+                </div>
+            </div>
+
+        </section></div>
+        <div class="reclame_rechts reclame">
+          <div class="media_uiting">
+            <section><h3>Reclame</h3><article>Hier wat geschreven reclame blaat blaat blaat.</article></section>
+          </div>
+
+          <div class="media_uiting">
+            <section><h3>Reclame 2</h3><article>Hier wat geschreven reclame blaat blaat blaat.</article></section>
+          </div>
+
+        </div>
+        <div class="footer">'t Vette Bekkie 2025&copy;&nbsp; | &nbsp; Lees onze <a href='#'>verwerkingsovereenkomst</a></div>
+    </div>
+</body>
+</html>
+
